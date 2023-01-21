e9ac25a431558eaf7072d38d51fb01e69537a198
kauelima21 <kaueslim@outlook.com>
2023-01-21 17:17:31 -0300

    send to github

---
+++ src/app.routing.ts
@@ -0,0 +1,15 @@
+import { Route, provideRouter } from "@angular/router";
+
+const routes: Route[] = [
+  {
+    path: "",
+    loadComponent: () =>
+      import("./home.component").then((c) => c.HomeComponent),
+  },
+  {
+    path: "about",
+    loadComponent: (): any =>
+      import("./about.component").then((c) => c.AboutComponent),
+  },
+];
+export const appRouting = [provideRouter(routes)];
